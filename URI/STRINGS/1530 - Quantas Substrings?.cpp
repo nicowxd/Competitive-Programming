ce0d4b4d1cef2916fd1f707dc58fb0419f67cf1b
CarlosJunior <carlos.serrato@itec.ufpa.br>
2018-02-10 14:23:33 -0300

    Array instead of map

--- URI/STRINGS/1530 - Quantas Substrings?.cpp
+++ URI/STRINGS/1530 - Quantas Substrings?.cpp
@@ -6,7 +6,7 @@ const int MAXN = 2e5 + 10;
 
 struct state {
 	int len, link;
-	map<char, int> next;
+	int next[26];
 };
 
 
@@ -20,7 +20,7 @@ void sa_init()
 	sz = 1;
 	st[0].link = -1;
 	st[0].len = 0;
-	st[0].next.clear();
+	memset(st[0].next, 0, sizeof st[0].next);
 	ans = 0;
 }
 
@@ -28,17 +28,17 @@ void sa_extend (char ch)
 {
 	int cur = sz++;
 	st[cur].len = st[last].len + 1;
-	st[cur].next.clear();
+	memset(st[cur].next, 0, sizeof st[cur].next);
 
 	int p;
-	for (p = last; p != -1 && !st[p].next.count(ch); p = st[p].link)
-		st[p].next[ch] = cur;
+	for (p = last; p != -1 && st[p].next[ch-'a'] == 0; p = st[p].link)
+		st[p].next[ch-'a'] = cur;
 	
 	if (p == -1)
 		st[cur].link = 0;
 	else
 	{
-		int q = st[p].next[ch];
+		int q = st[p].next[ch-'a'];
 
 		if (st[p].len + 1 == st[q].len)
 			st[cur].link = q;
@@ -47,9 +47,10 @@ void sa_extend (char ch)
 			int clone = sz++;
 			st[clone].len = st[p].len + 1;
 			st[clone].link = st[q].link;
-			st[clone].next = st[q].next;
-			for (; p != -1 && st[p].next[ch] == q; p = st[p].link)
-				st[p].next[ch] = clone;
+			for (int i = 0; i < 26; i++)
+				st[clone].next[i] = st[q].next[i];
+			for (; p != -1 && st[p].next[ch-'a'] == q; p = st[p].link)
+				st[p].next[ch-'a'] = clone;
 			st[q].link = st[cur].link = clone;
 		}
 	}
