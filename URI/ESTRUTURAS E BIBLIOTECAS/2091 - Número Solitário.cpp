667b9386b2a2f1dea711ac3c6636c10c0fa7be84
CarlosJunior <carlos.serrato@itec.ufpa.br>
2018-03-13 16:12:13 -0300

    Map usage to count, then verify the number which count is odd

---
+++ URI/ESTRUTURAS E BIBLIOTECAS/2091 - Número Solitário.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+
+    while(scanf("%d", &n) && n)
+    {
+    	long long x;
+        unordered_map<long long, int> m;
+        
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%lld", &x);
+            m[x]++;
+        }
+
+        for (auto x : m)
+            if (x.second&1)
+                cout << x.first << endl;
+    }
+
+    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    return 0;
+}
