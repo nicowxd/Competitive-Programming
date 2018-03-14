b1a2d33d1ef4eb1cfbbe426569431d7934988340
CarlosJunior <carlos.serrato@itec.ufpa.br>
2018-03-14 18:45:00 -0300

    Counting sort algorithm

--- URI/ESTRUTURAS E BIBLIOTECAS/1566 - Altura.cpp
+++ URI/ESTRUTURAS E BIBLIOTECAS/1566 - Altura.cpp
@@ -1,39 +1,45 @@
-// Autor: CarlosJunior<carloserratojr@gmail.com>
-// Nome: Altura
-// Nível: 2
-// Categoria: ESTRUTURAS E BIBLIOTECAS
-// URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1566
-
-#include <iostream>
-#include <cstdio>
-#include <algorithm> 
+#include <bits/stdc++.h>
 
 using namespace std;
 
+const int MAXN = 3000010;
+
+int A[MAXN], B[MAXN];
+
 int main()
 {
-	int nc,n,altura[3000010];
-	scanf("%d", &nc);
-	for (int i = 0; i < nc; i++)
-	{
-	scanf("%d", &n);
-	for (int j = 0; j < n; j++)
-	{
-	scanf("%d", &altura[j]);
-	}
-	sort(altura,altura+n);
-	for (int j = 0; j < n; j++)
-	{
-	if (j == n-1)
-	{
-	printf("%d\n", altura[j]);
-	}
-	else
-	{
-	printf("%d ", altura[j]);
-	}
-	}
-	}
-	return 0;
+
+    int nc;
+    scanf("%d", &nc);
+
+    while(nc--)
+    {
+        int n;
+        scanf("%d", &n);
+        
+        memset(A, 0, sizeof A);
+        for (int i = 0; i < n; i++)
+            scanf("%d", A + i);
+        
+        int C[240];
+        
+        memset(B, 0, sizeof B);
+        for (int i = 0; i <= 230; i++)
+            C[i] = 0;
+        for (int i = 0; i < n; i++)
+            C[A[i]]++;
+        for (int i = 1; i <= 230; i++)
+            C[i] = (C[i] + C[i-1]);
+        for (int i = n-1; i >= 0; i--)
+        {   
+            B[C[A[i]]] = A[i];
+            C[A[i]]--;
+        }
+        
+        for (int i = 1; i <= n; i++)
+            printf("%d%c", B[i], (i == n) ? '\n' : ' ');
+    }
+
+    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    return 0;
 }
-	
